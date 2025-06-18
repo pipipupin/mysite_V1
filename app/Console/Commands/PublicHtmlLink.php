92f0125a2c19f363b161385ff6a30b49c26674b3
pipipupin <necochan23@yandex.ru>
2025-06-18 19:37:18 +0500

    Добавил artisan-команду и composer.phar

---
+++ app/Console/Commands/PublicHtmlLink.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PublicHtmlLink extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:public-html-link';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
